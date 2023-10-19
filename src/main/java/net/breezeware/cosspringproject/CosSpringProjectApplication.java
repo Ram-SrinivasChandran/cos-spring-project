d91465dbfe325a391b5712053a35466d18d0384a
Seenu <chand2ram@gmail.com>
2023-10-19 15:56:30 +0530

    Project intiation

---
+++ src/main/java/net/breezeware/cosspringproject/CosSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package net.breezeware.cosspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/breezeware/cosspringproject/CosSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.breezeware.cosspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
